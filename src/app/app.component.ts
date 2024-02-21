b2d295bdd170065df6046b145a040edf645a5669
abhay <abhay.nepal.9@gmail.com>
2024-02-22 00:10:39 +0545

    added form array and alternative emails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,80 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject, numberAttribute } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import {forbiddenNameValidator} from './shared/username-validator'
+import { FormBuilder, FormGroup, FormArray, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
+import { forbiddenNameValidator } from './shared/username-validator'
 import { passwordValidator } from './shared/password.validator';
- 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  formBuilder:FormBuilder = inject(FormBuilder)
+  formBuilder: FormBuilder = inject(FormBuilder)
+  registrationForm!: FormGroup;
 
-  get userName(){
+  get userName() {
     return this.registrationForm.controls?.['userName']
   }
 
-loadAPIData() {
-  this.registrationForm.patchValue({
-    userName:'Bruce',
-    address:{
-      city:'city',
-      state:'state',
-      postalCode:'34343'
-    }
-  });
-}
+  get email() {
+    return this.registrationForm.controls?.['email']
+  }
+  get alternateEmails() {
+    return this.registrationForm.controls?.['alternateEmails'] as FormArray;
+  }
 
-registrationForm= this.formBuilder.group({
-  userName:['',[Validators.required,Validators.minLength(3),forbiddenNameValidator(/password/)]],
-  password:[''],
-  confirmPassword : [''],
-  address: this.formBuilder.group({
-    city:[''],
-    state:[''],
-    postalCode:['']
-  })
-},{validator:passwordValidator})
+  addAlternateEmail() {
+    this.alternateEmails.push(this.formBuilder.control(''));
+  }
+
+  removeAlternativeEmail() {
+    this.alternateEmails.removeAt(this.alternateEmails.length-1)
+    }
+
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+      password: [''],
+      confirmPassword: [''],
+      email: [''],
+      subscribe: [false],
+      address: this.formBuilder.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      }),
+      alternateEmails: this.formBuilder.array([])
+    }, { validator: passwordValidator });
+    this.registrationForm.controls?.['subscribe'].valueChanges
+      .subscribe(checkedvalue => {
+        const email = this.registrationForm.controls?.['email'];
+        if (checkedvalue) {
+          email.setValidators(Validators.required)
+        } else {
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      })
+  }
+
+  loadAPIData() {
+    this.registrationForm.patchValue({
+      userName: 'Bruce',
+      address: {
+        city: 'city',
+        state: 'state',
+        postalCode: '34343'
+      }
+    });
+  }
+
+
   // registrationForm = new FormGroup({
   //   userName: new FormControl(''),
   //   password: new FormControl(''),
